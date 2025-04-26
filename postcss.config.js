46c23464cedd4e40588c2e1dbb72ab9bb48496cb
Viliam Schrojf <viliam@schrojf.sk>
2025-04-26 11:59:31 +0200

    Add support for older browser

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,8 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    "postcss-preset-env": {
+      browsers: 'cover 99.5%',
+    },
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    legacy({
+      targets: ['cover 99.5%'],
+    }),
+  ],
 })
